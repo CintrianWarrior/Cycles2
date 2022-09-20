90b80784488c23b9a1c9fdf84ff5b518a421d836
Cintrian <velegoj2@yandex.ru>
2022-09-20 23:06:21 +0300

    task2.3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-      double monthlyDeposit = 15000;
-      double totalDeposit = 0;
-      for (int month = 1; totalDeposit<12_000_000; month++){
-          monthlyDeposit = (monthlyDeposit + monthlyDeposit*0.07);
-          totalDeposit = (int) (totalDeposit + monthlyDeposit);
+      int monthlyDeposit = 15000;
+      int totalDeposit = 0;
+      for (int month = 1; month<=12*9; month++){
+          monthlyDeposit = (monthlyDeposit + monthlyDeposit*7/100);
+          totalDeposit = (totalDeposit + monthlyDeposit);
           if (month%6 ==0) {
               System.out.println("Месяц " + month + ", сумма накоплений равна " + totalDeposit + " рублей");
           }
