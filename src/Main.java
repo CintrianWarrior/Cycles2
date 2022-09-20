3fa244033a8a9904bba43e19eaf96590c64a13ba
Cintrian <velegoj2@yandex.ru>
2022-09-20 21:46:14 +0300

    task1.2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int monthlyDeposit = 15000;
-        int totalDeposit = 0;
         int i = 0;
-        while (totalDeposit<=2459000) {
+        while (i < 10) {
             i++;
-            totalDeposit = totalDeposit + totalDeposit/100;
-            totalDeposit = totalDeposit + monthlyDeposit;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalDeposit + " рублей");
+            System.out.print(i + " ");
         }
-        System.out.println(i + " месяцев необходимо для накопления суммы");
+        System.out.println();
+        for (; i>0; i--) {
+            System.out.print(i + " ");
+        }
     }
 }
