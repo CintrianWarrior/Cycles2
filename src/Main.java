55ea77cad31ed6799defdaf94751e1954bae9340
Cintrian <velegoj2@yandex.ru>
2022-09-21 00:04:06 +0300

    task3.1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-      int friday = 2;
-        System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
-       for (int i=1; i<30; i++){
-           if (i%7==2) {
-               friday=friday+7;
-               System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
-           }
+      for(int year = 2022-200; year<2022+100; year++) {
+          if (year%79==0) {
+              System.out.println(year);
+          }
       }
     }
 }
