1a0c2496bdf60d7b95c4d3e8061852c123fa2969
Cintrian <velegoj2@yandex.ru>
2022-09-20 22:53:34 +0300

    task2.1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-      int population = 12_000_000;
-      double birthRate = (double) 17/1000;
-      double deathRate = (double) 8/1000;
-      for (int i = 1; i<=10; i++) {
-          population = (int) (population + (population*birthRate) - (population*deathRate));
-          System.out.println("Год " + i + ", численность населения составляет " + population);
+      double monthlyDeposit = 15000;
+      double totalDeposit = 0;
+      for (int month = 1; totalDeposit<12_000_000; month++){
+          monthlyDeposit = (monthlyDeposit + monthlyDeposit*0.07);
+          totalDeposit = (int) (totalDeposit + monthlyDeposit);
+          System.out.println("Месяц " + month + ", сумма накоплений равна " + totalDeposit + " рублей");
       }
     }
 }
